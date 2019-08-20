61041002d6620de02595cbb3fbda5c2daa280fd2
Paul Frank Pacheco Carpio <paul.frankpc@gmail.com>
2019-08-19 21:08:43 -0500

    Add project files.

---
+++ Semana02-Propuesta03/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Semana02_Propuesta03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxYear = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxMonth = new System.Windows.Forms.ComboBox();
+            this.dataGridOrders = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxYear
+            // 
+            this.comboBoxYear.FormattingEnabled = true;
+            this.comboBoxYear.Location = new System.Drawing.Point(165, 131);
+            this.comboBoxYear.Name = "comboBoxYear";
+            this.comboBoxYear.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxYear.TabIndex = 0;
+            this.comboBoxYear.SelectionChangeCommitted += new System.EventHandler(this.comboBoxYear_SelectionChangeCommitted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Quicksand Medium", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(158, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(461, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lista de pedidos por año y mes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Quicksand Medium", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(188, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "año";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Quicksand Medium", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(480, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 41);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "mes";
+            // 
+            // comboBoxMonth
+            // 
+            this.comboBoxMonth.FormattingEnabled = true;
+            this.comboBoxMonth.Location = new System.Drawing.Point(460, 131);
+            this.comboBoxMonth.Name = "comboBoxMonth";
+            this.comboBoxMonth.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxMonth.TabIndex = 4;
+            this.comboBoxMonth.SelectionChangeCommitted += new System.EventHandler(this.comboBoxMonth_SelectionChangeCommitted);
+            // 
+            // dataGridOrders
+            // 
+            this.dataGridOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridOrders.Location = new System.Drawing.Point(12, 187);
+            this.dataGridOrders.Name = "dataGridOrders";
+            this.dataGridOrders.RowTemplate.Height = 24;
+            this.dataGridOrders.Size = new System.Drawing.Size(776, 251);
+            this.dataGridOrders.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridOrders);
+            this.Controls.Add(this.comboBoxMonth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxYear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxYear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxMonth;
+        private System.Windows.Forms.DataGridView dataGridOrders;
+    }
+}
+
---
+++ Semana02-Propuesta03/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Semana02_Propuesta03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["kotoha"].ConnectionString);
+
+        public void fillComboYear()
+        {
+            using (SqlCommand command = new SqlCommand("usp_years_orders", connection))
+            {
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                {
+                    dataAdapter.SelectCommand = command;
+                    dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    comboBoxYear.DataSource = dataTable;
+                    comboBoxYear.DisplayMember = "Years";
+                    comboBoxYear.ValueMember = "order_years";
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fillComboYear();
+        }
+
+        private void comboBoxYear_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            int codigo;
+            codigo = Convert.ToInt32(comboBoxYear.SelectedValue);
+            using (SqlCommand command = new SqlCommand("usp_month_orders_2", connection))
+            {
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                {
+                    dataAdapter.SelectCommand = command;
+                    dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    dataAdapter.SelectCommand.Parameters.AddWithValue("@orderYear", codigo);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    comboBoxMonth.DataSource = dataTable;
+                    comboBoxMonth.DisplayMember = "month_name";
+                    comboBoxMonth.ValueMember = "month_number";
+                }
+            }
+        }
+
+        private void comboBoxMonth_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            int year, month;
+            year = Convert.ToInt32(comboBoxYear.SelectedValue);
+            month = Convert.ToInt32(comboBoxMonth.SelectedValue);
+            using (SqlCommand command = new SqlCommand("usp_filter_by_month_and_date", connection))
+            {
+                using (SqlDataAdapter dataAdapter = new SqlDataAdapter())
+                {
+                    dataAdapter.SelectCommand = command;
+                    dataAdapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    dataAdapter.SelectCommand.Parameters.AddWithValue("@orderYear", year);
+                    dataAdapter.SelectCommand.Parameters.AddWithValue("@orderMonth", month);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+                    dataGridOrders.DataSource = dataTable;
+                }
+            }
+        }
+    }
+}
